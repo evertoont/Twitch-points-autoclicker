3d834628dee6c159a16d2b906d782630299b9022
Everton Carmo <evertoontcarmo@gmail.com>
2021-04-13 16:03:21 -0300

    Create function openBrowser and add Path for chrome and user data chrome

--- twitch_collect.js
+++ twitch_collect.js
@@ -1,2 +1,22 @@
+const CHROME_PATH = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+const PROFILE_PATH = `${process.env['USERPROFILE']}\\AppData\\Local\\Google\\Chrome\\User Data`;
+
 const puppeteer = require("puppeteer-core");
 const readlineSync = require("readline-sync");
+
+async function openBrowser() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    executablePath: CHROME_PATH,
+    userDataDir: PROFILE_PATH,
+    args: ["--disable-extensions"],
+  });
+
+  const page = await browser.newPage();
+  page.setDefaultNavigationTimeout(0);
+  await page.goto('https://www.twitch.tv/');
+
+}
+
+openBrowser()
