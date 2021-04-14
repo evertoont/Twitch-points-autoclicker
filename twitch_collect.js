461bf010a6feb6136ee50b82395ff6b4a4635151
Everton Carmo <evertoontcarmo@gmail.com>
2021-04-14 16:07:42 -0300

    Create function to click on the chest

--- twitch_collect.js
+++ twitch_collect.js
@@ -1,10 +1,11 @@
 const CHROME_PATH = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
-const PROFILE_PATH = `${process.env['USERPROFILE']}\\AppData\\Local\\Google\\Chrome\\User Data`;
+const PROFILE_PATH = `${process.env["USERPROFILE"]}\\AppData\\Local\\Google\\Chrome\\User Data`;
 
 const puppeteer = require("puppeteer-core");
 const readlineSync = require("readline-sync");
 
 let channel = readlineSync.question("https://www.twitch.tv/");
+let amountOfCollect = 0;
 
 async function openBrowser() {
   const browser = await puppeteer.launch({
@@ -19,6 +20,22 @@ async function openBrowser() {
   page.setDefaultNavigationTimeout(0);
   await page.goto(`https://www.twitch.tv/${channel}`);
 
+  console.log("Waiting to collect...");
+
+  collectPoints(page);
 }
 
-openBrowser()
+async function collectPoints(page) {
+  setInterval(() => {
+    page.evaluate(() => {
+      let chestPoints = document.querySelector(
+        ".community-points-summary .tw-z-above button.tw-button"
+      );
+      chestPoints.click();
+    });
+
+    console.log(`Bonux Chest Clicked. [${(amountOfCollect += 1)}]`);
+  }, 960000);
+}
+
+openBrowser();
