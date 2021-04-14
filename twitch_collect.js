fbbefcfbafacb0ed3969324f19fa3c459269d2bd
Everton Carmo <evertoontcarmo@gmail.com>
2021-04-14 17:32:07 -0300

    Create responsible function for returning the collection time

--- twitch_collect.js
+++ twitch_collect.js
@@ -9,7 +9,7 @@ let amountOfCollect = 0;
 
 async function openBrowser() {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: null,
     executablePath: CHROME_PATH,
     userDataDir: PROFILE_PATH,
@@ -34,8 +34,17 @@ async function collectPoints(page) {
       chestPoints.click();
     });
 
-    console.log(`Bonux Chest Clicked. [${(amountOfCollect += 1)}]`);
+    console.log(`[${amountOfCollect += 1}] Bonus Chest Clicked. ` + timeString());
   }, 960000);
+}
+
+function timeString() {
+  let date = new Date();
+
+  hour = date.getHours();
+  minute = date.getMinutes();
+
+  return hour + ":" + minute;
 }
 
 openBrowser();
